4b28bc0a0a5b131ce16b5d32cffe6e0ef155c70c
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:02 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='checklist'
+    name='procedures'
+    title='proceduresProcedures'
+    {...props}
+/>
